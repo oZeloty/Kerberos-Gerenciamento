7eb5707ffaaeebe7040c19d1b7d80f133e1e6dc3
Matheus N <matheusnunesdemoraes432@gmail.com>
2026-07-01 20:54:11 +0000

    Configurando urls, admin  e SQLlite

--- ProjetoFamaPet/FamaPet/settings.py
+++ ProjetoFamaPet/FamaPet/settings.py
@@ -117,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://localhost:8000",
+]
--- ProjetoFamaPet/kerberos/admin.py
+++ ProjetoFamaPet/kerberos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import (Endereco, Pet, Agendamento, Servico, ServicoAgendamento, Usuario)
 
-# Register your models here.
+admin.site.register(Endereco)
+admin.site.register(Pet)
+admin.site.register(Agendamento)
+admin.site.register(Servico)
+admin.site.register(ServicoAgendamento)
+admin.site.register(Usuario)
